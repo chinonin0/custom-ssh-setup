32e7d9ce2bb82c7295799b1d5cdbddb4da333d1e
Victor Bravo <63818704+chinonin0@users.noreply.github.com>
2025-11-11 15:19:46 +0100

    Update setup-test.sh

--- setup-test.sh
+++ setup-test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-touch /tmp/pepito
-date > /tmp/pepito
+set -e
+#apt update && apt upgrade -y
+apt update
+apt -y install apache2
+systemctl start apache2
+systemctl enable apache2
+sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sed -i 's/^#*PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
+sed -i 's/^#*AllowTcpForwarding.*/AllowTcpForwarding yes/' /etc/ssh/sshd_config
+systemctl restart sshd
